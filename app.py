ea661b951c74d76cd84679f18131987de5350deb
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-09-06 10:41:41 +0000

    1. added "not found" page 2. added using HTML template

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask, request
+from flask import Flask, request, render_template
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -27,10 +29,15 @@ def greet():
     with open('submissions.txt', 'a') as file:
         # Write a string to the file
         file.write((f"Hello {name}!\n"))
-
+    '''
+    # variant 1: inline approach 
     return f'\
         <h3>Hello, {name}!</h3>\
         <a href="javascript:history.back()">back<a>'
+    '''
+    # variant 2: template (requires render_template)
+    return render_template('hello.html', name=name)
+
 
 @app.route("/log")
 def submissions_log():
@@ -52,3 +59,10 @@ def submissions_log():
     
     print(content)
     return content
+
+'''
+@app.route("/<name>")
+def hello(name):
+    return f"<p>Page <b>{escape(name)}</b> not found!</p>"
+'''
+
